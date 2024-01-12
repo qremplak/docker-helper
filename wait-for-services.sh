3a47bf66e58ac8bed103d379e7c8192da4245ed6
qremplak <q.rempla@gmail.com>
2024-01-12 17:11:02 +0100

    handle any service type

--- wait-for-services.sh
+++ wait-for-services.sh
@@ -8,32 +8,45 @@
 
 T=3
 
-# store services & patterns by iterating over arguments 2 by 2
-services=()
-patterns=()
+# store partA & partB by iterating over arguments 2 by 2
+list_partA=()
+list_partB=()
 for ((i=1; i<=$#; i+=2)); do
-    services+=("${!i}")
+    list_partA+=("${!i}")
     j=$(($i + 1))
-    patterns+=("${!j}")
+    list_partB+=("${!j}")
 done 
 
-echo ${services[@]}
-echo ${patterns[@]}
+echo ${list_partA[@]}
+echo ${list_partB[@]}
 
 all_services_ready=false
 while ! $all_services_ready; do
     clear
     all_services_ready=true
-    for ((i = 0; i < ${#services[@]}; i++)); do
-        url="${services[i]}"
-        pattern="${patterns[i]}"
-        if [ -z "$pattern" ]; then
-            echo -e "[--] $url"
-        elif curl --silent $url | grep -E -q $pattern; then
-            echo -e "[OK] $url"
+    for ((i = 0; i < ${#list_partA[@]}; i++)); do
+        partA="${list_partA[i]}"
+        partB="${list_partB[i]}"
+        if [ -z "$partB" ]; then
+            echo -e "[--] $partA"
+        elif [[ $partA == *http://* ]]; then
+            url=$partA
+            pattern=$partB
+            if curl --silent $url | grep -E -q $pattern; then
+                echo -e "[OK] $url"
+            else
+                echo -e "[KO] $url"
+                all_services_ready=false
+            fi
         else
-            echo -e "[KO] $url"
-            all_services_ready=false
+            host=$partA
+            port=$partB
+            if nc -zw3 $host $port; then
+                echo -e "[OK] $host:$port"
+            else
+                echo -e "[KO] $host:$port"
+                all_services_ready=false
+            fi
         fi
     done
     sleep $T
